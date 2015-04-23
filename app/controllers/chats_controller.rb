b69f95f9783488b1fe64aaf0d077f2d038f9c447
Haru Okaniwa <haruokny@gmail.com>
2015-04-23 10:24:52 +0900

    [modify] chats controller changed channel to test

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,10 +1,13 @@
 class ChatsController < ApplicationController
 
 def post
-  Pusher['general_channel'].trigger('chat_event', {
-  message: params[:message]
-  })
-  render :text => 'OK', :status => 200
+    Pusher['test_channel'].trigger('my_event', {
+      message: 'hello world'
+    })
+  # Pusher['general_channel'].trigger('chat_event', {
+  # message: params[:message]
+  # })
+  # render :text => 'OK', :status => 200
 end
 
   def show
